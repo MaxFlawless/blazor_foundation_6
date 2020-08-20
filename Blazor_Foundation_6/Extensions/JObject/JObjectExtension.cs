7dd4527466d9370a6e3ab70ab7db8845a5cd0bdc
Max Samson <m.samson@flawlessloop.com>
2020-08-20 19:35:06 -0400

    Update Documentation. Add Style Attribute to All Elements

---
+++ Blazor_Foundation_6/Extensions/JObject/JObjectExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Newtonsoft.Json.Linq
+{
+    public static class JObjectExtension
+    {
+        public static string ToInlineCSS(this JObject jo)
+        {
+            string css = "";
+            foreach (var item in jo)
+            {
+                css += $"{item.Key}:{item.Value};";
+            }
+            return css;
+        }
+    }
+}
