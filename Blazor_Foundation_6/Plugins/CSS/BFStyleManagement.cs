03084a75d4f85d8dac3a3e419f7a43e152ffc5c8
Max Samson <m.samson@flawlessloop.com>
2020-10-24 19:31:42 -0400

    Update Styale Management System

---
+++ Blazor_Foundation_6/Plugins/CSS/BFStyleManagement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Foundation_6.Plugins.CSS
+{
+    public class BFStyleManagement
+    {
+        public static string PrimaryColor { get; set; } = "#DF682D";
+        public static string PrimaryColorDisabled { get; set; } = "#DF682D";
+        public static string SecondaryColor { get; set; } = "#e9e9e9";
+        public static string SecondaryColorDisabled { get; set; } = "#e9e9e9";
+        public static string AlertColor { get; set; } = "#c60f13";
+        public static string AlertColorDisabled { get; set; } = "#c60f13";
+        public static string SuccessColor { get; set; } = "#5da423";
+        public static string SuccessColorDisabled { get; set; } = "#5da423";
+        public static string WarningColor { get; set; } = "#ffae00";
+        public static string WarningColorDisabled { get; set; } = "#ffae00";
+
+        public static string BodyFontColor { get; set; } = "#000000";
+        public static string BodyColor { get; set; } = "#ffffff";
+        public static string HeaderFontColor { get; set; } = "#222222";
+        public static string HeaderColor { get; set; } = "#222222";
+    }
+}
